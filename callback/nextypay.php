36e5eaf63f264713b3330ef105f738a2edb8fddb
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-08 16:11:53 +0700

    add params into backend setting to connect with gateway

---
+++ callback/nextypay.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * WHMCS Nextypay Callback File
+ *
+ * This sample file demonstrates how a payment with Nextypay callback should be
+ * handled within WHMCS.
+ *
+ * It demonstrates verifying that the payment with Nextypay module is active,
+ * validating an Invoice ID, checking for the existence of a Transaction ID,
+ * Logging the Transaction for debugging and Adding Payment to an Invoice.
+ *
+ * For more information, please refer to the online documentation.
+ *
+ * @copyright Copyright (c) WHMCS Limited 2017
+ * @license http://www.whmcs.com/license/ WHMCS Eula
+ */
+// Require libraries needed for Nextypay module functions.
+require_once __DIR__ . '/../../../init.php';
+require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
+require_once __DIR__ . '/../../../includes/invoicefunctions.php';
+// Detect module name from filename.
+$nextypayModuleName = basename(__FILE__, '.php');
+// Fetch Nextypay configuration parameters.
+$nextypayParams = getGatewayVariables($nextypayModuleName);
+// Die if module is not active.
+if (!$nextypayParams['type']) {
+    die("Module Not Activated");
+}
+// Retrieve data returned in payment with Nextypay callback
+// Varies per payment with Nextypay
+$success = ($_POST["status"] == 'Paid');
+$invoiceId = $_POST["orderId"];
+$transactionId = $_POST["reqId"];
+$paymentAmount = $_POST["amount"];
+$paymentFee = $_POST["fee"];
+$hash = $_POST["hash"];
+$transactionStatus = $success ? 'Success' : 'Failure';
+/**
+ * Validate callback authenticity.
+ *
+ * Most payment gateways provide a method of verifying that a callback
+ * originated from them. In the case of our example here, this is achieved by
+ * way of a shared secret which is used to build and compare a hash.
+ */
+//echo $_POST;
+$secretKey = $nextypayParams['secretKey'];
+//$hash = md5($transactionId . $invoiceId . $paymentAmount . $secretKey);
+//echo $hash;
+//echo $hash.'<br>';
+//echo  md5($invoiceId . $transactionId . $paymentAmount . $secretKey);
+if ($hash != md5($invoiceId . $transactionId . $paymentAmount . $secretKey)) {
+    $transactionStatus = 'Hash Verification Failure';
+    $success = false;
+}
+/**
+ * Validate Callback Invoice ID.
+ *
+ * Checks invoice ID is a valid invoice number. Note it will count an
+ * invoice in any status as valid.
+ *
+ * Performs a die upon encountering an invalid Invoice ID.
+ *
+ * Returns a normalised invoice ID.
+ *
+ * @param int $invoiceId Invoice ID
+ * @param string $gatewayName Gateway Name
+ */
+$invoiceId = checkCbInvoiceID($invoiceId, $nextypayParams['name']);
+/**
+ * Check Callback Transaction ID.
+ *
+ * Performs a check for any existing transactions with the same given
+ * transaction number.
+ *
+ * Performs a die upon encountering a duplicate.
+ *
+ * @param string $transactionId Unique Transaction ID
+ */
+checkCbTransID($transactionId);
+/**
+ * Log Transaction.
+ *
+ * Add an entry to the Gateway Log for debugging purposes.
+ *
+ * The debug data can be a string or an array. In the case of an
+ * array it will be
+ *
+ * @param string $gatewayName        Display label
+ * @param string|array $debugData    Data to log
+ * @param string $transactionStatus  Status
+ */
+logTransaction($nextypayParams['name'], $_POST, $transactionStatus);
+if ($success) {
+    /**
+     * Add Invoice Payment.
+     *
+     * Applies a payment transaction entry to the given invoice ID.
+     *
+     * @param int $invoiceId         Invoice ID
+     * @param string $transactionId  Transaction ID
+     * @param float $paymentAmount   Amount paid (defaults to full balance)
+     * @param float $paymentFee      Payment fee (optional)
+     * @param string $nextypayModule  Gateway module name
+     */
+    addInvoicePayment(
+        $invoiceId,
+        $transactionId,
+        $paymentAmount,
+        $paymentFee,
+        $nextypayModuleName
+    );
+    echo 'Ok';
+}
---
+++ nextypay.php
@@ -0,0 +1,336 @@
+<?php
+/**
+ * WHMCS Nextypay Module
+ *
+ * Payment Gateway modules allow you to integrate payment solutions with the
+ * WHMCS platform.
+ *
+ * This sample file demonstrates how a payment with Nextypay module for WHMCS should
+ * be structured and all supported functionality it can contain.
+ *
+ * Within the module itself, all functions must be prefixed with the module
+ * filename, followed by an underscore, and then the function name. For this
+ * example file, the filename is "nextypay" and therefore all functions
+ * begin "nextypay_".
+ *
+ * If your module or third party API does not support a given function, you
+ * should not define that function within your module. Only the _config
+ * function is required.
+ *
+ * For more information, please refer to the online documentation.
+ *
+ * @copyright Copyright (c) WHMCS Limited 2017
+ * @license http://www.whmcs.com/license/ WHMCS Eula
+ */
+
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+
+/**
+ * Define module related meta data.
+ *
+ * Values returned here are used to determine module related capabilities and
+ * settings.
+ * @return array
+ */
+function nextypay_MetaData()
+{
+    return array(
+        'DisplayName' => 'Nextypay',
+        'APIVersion' => '1.1', // Use API Version 1.1
+        'DisableLocalCredtCardInput' => true,
+        'TokenisedStorage' => false,
+    );
+}
+
+/**
+ * Define gateway configuration options.
+ *
+ * The fields you define here determine the configuration options that are
+ * presented to administrator users when activating and configuring your
+ * payment with Nextypay for use.
+ *
+ * Supported field types include:
+ * * text
+ * * password
+ * * yesno
+ * * dropdown
+ * * radio
+ * * textarea
+ *
+ * Examples of each field type and their possible configuration parameters are
+ * provided in the sample function below.
+ *
+ * @return array
+ */
+function nextypay_config()
+{
+    return array(
+        // the friendly display name for a payment with Nextypay should be
+        // defined here for backwards compatibility
+        'FriendlyName' => array(
+            'Type' => 'System',
+            'Value' => 'Nextypay',
+        ),
+        // a text field type allows for single line text input
+        // 'accountID' => array(
+        //     'FriendlyName' => 'Account ID',
+        //     'Type' => 'text',
+        //     'Size' => '25',
+        //     'Default' => '',
+        //     'Description' => 'Enter your account ID here',
+        // ),
+        // a password field type allows for masked text input
+        'shopId' => array(
+            'FriendlyName' => 'Shop ID',
+            'Type' => 'text',
+            'Size' => '10',
+            'Default' => '0', //nextypay gateway
+            'Description' => 'Enter shop ID here',
+        ),
+        'gatewayAddress' => array(
+            'FriendlyName' => 'Gateway Address',
+            'Type' => 'text',
+            'Size' => '100',
+            'Default' => 'https://localhost/Nextypay-gateway/request.php', //nextypay gateway
+            'Description' => 'Enter gateway address here',
+        ),
+        // a password field type allows for masked text input
+        'walletAddress' => array(
+            'FriendlyName' => 'Wallet Address',
+            'Type' => 'text',
+            'Size' => '42',
+            'Default' => '0x21a790077be4f722a9bd773b7b78f7e7c2cc6e42',
+            'Description' => 'Enter wallet address here',
+        ),
+        // a password field type allows for masked text input
+        'secretKey' => array(
+            'FriendlyName' => 'Secret Key',
+            'Type' => 'password',
+            'Size' => '32',
+            'Default' => 'Root123',
+            'Description' => 'Enter secret key here',
+        ),
+        // the yesno field type displays a single checkbox option
+        // 'testMode' => array(
+        //     'FriendlyName' => 'Test Mode',
+        //     'Type' => 'yesno',
+        //     'Description' => 'Tick to enable test mode',
+        // ),
+        // the dropdown field type renders a select menu of options
+        'dropdownField' => array(
+            'FriendlyName' => 'Dropdown Field',
+            'Type' => 'dropdown',
+            'Options' => array(
+                'option1' => 'Display Value 1',
+                'option2' => 'Second Option',
+                'option3' => 'Another Option',
+            ),
+            'Description' => 'Choose one',
+        ),
+        // the radio field type displays a series of radio button options
+        'radioField' => array(
+            'FriendlyName' => 'Radio Field',
+            'Type' => 'radio',
+            'Options' => 'First Option,Second Option,Third Option',
+            'Description' => 'Choose your option!',
+        ),
+        // the textarea field type allows for multi-line text input
+        'textareaField' => array(
+            'FriendlyName' => 'Textarea Field',
+            'Type' => 'textarea',
+            'Rows' => '3',
+            'Cols' => '60',
+            'Description' => 'Freeform multi-line text input field',
+        ),
+    );
+}
+
+/**
+ * Payment link.
+ *
+ * Required by third party payment with Nextypay only.
+ *
+ * Defines the HTML output displayed on an invoice. Typically consists of an
+ * HTML form that will take the user to the payment with Nextypay endpoint.
+ *
+ * @param array $params Payment Gateway Module Parameters
+ *
+ * @return string
+ */
+function nextypay_link($params)
+{
+    // Gateway Configuration Parameters
+    $accountId = $params['accountID'];
+    $secretKey = $params['secretKey'];
+    $testMode = $params['testMode'];
+    $dropdownField = $params['dropdownField'];
+    $radioField = $params['radioField'];
+    $textareaField = $params['textareaField'];
+
+    // Invoice Parameters
+    $invoiceId = $params['invoiceid'];
+    $description = $params["description"];
+    $amount = $params['amount'];
+    $currencyCode = $params['currency'];
+
+    // Client Parameters
+    $firstname = $params['clientdetails']['firstname'];
+    $lastname = $params['clientdetails']['lastname'];
+    $email = $params['clientdetails']['email'];
+    $address1 = $params['clientdetails']['address1'];
+    $address2 = $params['clientdetails']['address2'];
+    $city = $params['clientdetails']['city'];
+    $state = $params['clientdetails']['state'];
+    $postcode = $params['clientdetails']['postcode'];
+    $country = $params['clientdetails']['country'];
+    $phone = $params['clientdetails']['phonenumber'];
+
+    // System Parameters
+    $companyName = $params['companyname'];
+    $systemUrl = $params['systemurl'];
+    $returnUrl = $params['returnurl'];
+    $langPayNow = $params['langpaynow'];
+    $moduleDisplayName = $params['name'];
+    $moduleName = $params['paymentmethod'];
+    $whmcsVersion = $params['whmcsVersion'];
+    $mainUrl = 'coming soon!'; //need Server, Domain
+    $testUrl = 'https://localhost:3008';
+    $url = $params['gatewayAddress']; //to change
+    $wallet = $params['walletAddress'];
+    $toWallet = $wallet;
+    $shopId = $params['shopId'];
+
+
+    $postfields = array();
+    $postfields['username'] = $username;
+    $postfields['orderId'] = $invoiceId;
+    $postfields['shopId'] = $shopId;
+    $postfields['description'] = $description;
+    $postfields['amount'] = $amount;
+    $postfields['currency'] = $currencyCode;
+    $postfields['wallet'] = $wallet;
+    $postfields['currency'] = $currencyCode;
+    $postfields['first_name'] = $firstname;
+    $postfields['last_name'] = $lastname;
+    $postfields['email'] = $email;
+    $postfields['address1'] = $address1;
+    $postfields['address2'] = $address2;
+    $postfields['city'] = $city;
+    $postfields['state'] = $state;
+    $postfields['postcode'] = $postcode;
+    $postfields['country'] = $country;
+    $postfields['phone'] = $phone;
+    $postfields['callbackUrl'] = $systemUrl . '/modules/gateways/callback/' . $moduleName . '.php';
+    $postfields['returnUrl'] = $returnUrl;
+
+    $htmlOutput = '<form method="post" action="' . $url . '">';
+    foreach ($postfields as $k => $v) {
+        $htmlOutput .= '<input type="hidden" name="' . $k . '" value="' . urlencode($v) . '" />';
+    }
+    $htmlOutput .= '<input type="submit" value="' . $langPayNow . '" />';
+    $htmlOutput .= '</form>';
+
+    return $htmlOutput;
+}
+
+/**
+ * Refund transaction.
+ *
+ * Called when a refund is requested for a previously successful transaction.
+ *
+ * @param array $params Payment Gateway Module Parameters
+ *
+ * @return array Transaction response status
+ */
+function nextypay_refund($params)
+{
+    // Gateway Configuration Parameters
+    $accountId = $params['accountID'];
+    $secretKey = $params['secretKey'];
+    $testMode = $params['testMode'];
+    $dropdownField = $params['dropdownField'];
+    $radioField = $params['radioField'];
+    $textareaField = $params['textareaField'];
+
+    // Transaction Parameters
+    $transactionIdToRefund = $params['transid'];
+    $refundAmount = $params['amount'];
+    $currencyCode = $params['currency'];
+
+    // Client Parameters
+    $firstname = $params['clientdetails']['firstname'];
+    $lastname = $params['clientdetails']['lastname'];
+    $email = $params['clientdetails']['email'];
+    $address1 = $params['clientdetails']['address1'];
+    $address2 = $params['clientdetails']['address2'];
+    $city = $params['clientdetails']['city'];
+    $state = $params['clientdetails']['state'];
+    $postcode = $params['clientdetails']['postcode'];
+    $country = $params['clientdetails']['country'];
+    $phone = $params['clientdetails']['phonenumber'];
+
+    // System Parameters
+    $companyName = $params['companyname'];
+    $systemUrl = $params['systemurl'];
+    $langPayNow = $params['langpaynow'];
+    $moduleDisplayName = $params['name'];
+    $moduleName = $params['paymentmethod'];
+    $whmcsVersion = $params['whmcsVersion'];
+
+    // perform API call to initiate refund and interpret result
+
+    return array(
+        // 'success' if successful, otherwise 'declined', 'error' for failure
+        'status' => 'success',
+        // Data to be recorded in the gateway log - can be a string or array
+        'rawdata' => $responseData,
+        // Unique Transaction ID for the refund transaction
+        'transid' => $refundTransactionId,
+        // Optional fee amount for the fee value refunded
+        'fees' => $feeAmount,
+    );
+}
+
+/**
+ * Cancel subscription.
+ *
+ * If the payment with Nextypay creates subscriptions and stores the subscription
+ * ID in tblhosting.subscriptionid, this function is called upon cancellation
+ * or request by an admin user.
+ *
+ * @param array $params Payment Gateway Module Parameters
+ *
+ * @return array Transaction response status
+ */
+function nextypay_cancelSubscription($params)
+{
+    // Gateway Configuration Parameters
+    $accountId = $params['accountID'];
+    $secretKey = $params['secretKey'];
+    $testMode = $params['testMode'];
+    $dropdownField = $params['dropdownField'];
+    $radioField = $params['radioField'];
+    $textareaField = $params['textareaField'];
+
+    // Subscription Parameters
+    $subscriptionIdToCancel = $params['subscriptionID'];
+
+    // System Parameters
+    $companyName = $params['companyname'];
+    $systemUrl = $params['systemurl'];
+    $langPayNow = $params['langpaynow'];
+    $moduleDisplayName = $params['name'];
+    $moduleName = $params['paymentmethod'];
+    $whmcsVersion = $params['whmcsVersion'];
+
+    // perform API call to cancel subscription and interpret result
+
+    return array(
+        // 'success' if successful, any other value for failure
+        'status' => 'success',
+        // Data to be recorded in the gateway log - can be a string or array
+        'rawdata' => $responseData,
+    );
+}
